c79b6a66f310bf6474de6a1358a2257d8ca54768
Jacek Karolczak <jacek.karolczak@outlook.com>
2024-09-28 16:03:43 +0200

    Add docs and support for tree distance in 1-nn based metrics

--- protree/metrics/compare.py
+++ protree/metrics/compare.py
@@ -2,11 +2,24 @@ import numpy as np
 import pandas as pd
 
 from protree import TDataBatch, TPrototypes
+from protree.explainers.tree_distance import IExplainer
 from protree.explainers.utils import _type_to_np_dtype
 from protree.utils import parse_batch, parse_prototypes
 
 
 def get_euclidean_predictions(prototypes: TPrototypes, x: TDataBatch) -> np.ndarray:
+    """Generate predictions based on Euclidean distance between prototypes and the input data.
+
+    :param prototypes: Prototypes.
+    :type prototypes: TPrototypes
+    :param x: Input data.
+    :type x: TDataBatch
+
+    :returns: Assignment of the input data to classes based on Euclidean distance to the nearest prototype.
+    :rtype: np.ndarray
+
+    """
+
     prototypes = parse_prototypes(prototypes)
     x = parse_batch(x)
 
@@ -25,16 +38,133 @@ def get_euclidean_predictions(prototypes: TPrototypes, x: TDataBatch) -> np.ndar
     return predictions
 
 
-def mutual_information(a: TPrototypes, b: TPrototypes, x: TDataBatch) -> float:
+def _get_predictions(a: TPrototypes, b: TPrototypes, x: TDataBatch, explainer: IExplainer | None = None
+                     ) -> tuple[np.ndarray, np.ndarray]:
+    if explainer:
+        predictions_a = explainer.predict_with_prototypes(x, a)
+        predictions_b = explainer.predict_with_prototypes(x, b)
+    else:
+        predictions_a = get_euclidean_predictions(a, x)
+        predictions_b = get_euclidean_predictions(b, x)
+
+    return predictions_a, predictions_b
+
+
+def mutual_information(a: TPrototypes, b: TPrototypes, x: TDataBatch, explainer: IExplainer | None = None) -> float:
+    """Calculate the similarity metric based on mutual information between two sets of prototypes, with respect to the input
+    data. If an explainer is provided, the predictions are based on the explainer, otherwise, the predictions are based on
+    Euclidean distance.
+
+    :param a: First set of prototypes.
+    :type a: TPrototypes
+    :param b: Second set of prototypes.
+    :type b: TPrototypes
+    :param x: Input data.
+    :type x: TDataBatch
+    :param explainer: Explainer to generate predictions (Optional).
+    :type explainer: IExplainer
+
+    :returns: The similarity metric between the two sets of prototypes based on mutual information.
+    :rtype: float
+
+    """
+
     from sklearn.metrics import adjusted_mutual_info_score
 
-    predictions_a = get_euclidean_predictions(a, x)
-    predictions_b = get_euclidean_predictions(b, x)
+    predictions_a, predictions_b = _get_predictions(a, b, x, explainer)
 
     return adjusted_mutual_info_score(predictions_a, predictions_b, average_method="arithmetic")
 
 
+def rand_index(a: TPrototypes, b: TPrototypes, x: TDataBatch, explainer: IExplainer | None = None) -> float:
+    """Calculate the similarity metric based on the Rand index between two sets of prototypes, with respect to the input
+    data. If an explainer is provided, the predictions are based on the explainer, otherwise, the predictions are based on
+    Euclidean distance.
+
+    :param a: First set of prototypes.
+    :type a: TPrototypes
+    :param b: Second set of prototypes.
+    :type b: TPrototypes
+    :param x: Input data.
+    :type x: TDataBatch
+    :param explainer: Explainer to generate predictions (Optional).
+    :type explainer: IExplainer
+
+    :returns: The similarity metric between the two sets of prototypes based on the Rand index.
+    :rtype: float
+
+    """
+
+    from sklearn.metrics import adjusted_rand_score
+
+    predictions_a, predictions_b = _get_predictions(a, b, x, explainer)
+
+    return adjusted_rand_score(predictions_a, predictions_b)
+
+
+def fowlkes_mallows(a: TPrototypes, b: TPrototypes, x: TDataBatch, explainer: IExplainer | None = None) -> float:
+    """Calculate the similarity metric based on the Fowlkes-Mallows index between two sets of prototypes, with respect to the
+    input data. If an explainer is provided, the predictions are based on the explainer, otherwise, the predictions are based
+    on Euclidean distance.
+
+    :param a: First set of prototypes.
+    :type a: TPrototypes
+    :param b: Second set of prototypes.
+    :type b: TPrototypes
+    :param x: Input data.
+    :type x: TDataBatch
+    :param explainer: Explainer to generate predictions (Optional).
+    :type explainer: IExplainer
+
+    :returns: The similarity metric between the two sets of prototypes based on the Fowlkes-Mallows index.
+    :rtype: float
+
+    """
+
+    from sklearn.metrics import fowlkes_mallows_score
+
+    predictions_a, predictions_b = _get_predictions(a, b, x, explainer)
+
+    return fowlkes_mallows_score(predictions_a, predictions_b)
+
+
+def completeness(a: TPrototypes, b: TPrototypes, x: TDataBatch, explainer: IExplainer | None = None) -> float:
+    """Calculate the similarity metric based on the completeness score between two sets of prototypes, with respect to the
+    input data. If an explainer is provided, the predictions are based on the explainer, otherwise, the predictions are based
+    on Euclidean distance.
+
+    :param a: First set of prototypes.
+    :type a: TPrototypes
+    :param b: Second set of prototypes.
+    :type b: TPrototypes
+    :param x: Input data.
+    :type x: TDataBatch
+    :param explainer: Explainer to generate predictions (Optional).
+    :type explainer: IExplainer
+
+    :returns: The similarity metric between the two sets of prototypes based on the completeness score.
+    :rtype: float
+
+    """
+
+    from sklearn.metrics import completeness_score
+
+    predictions_a, predictions_b = _get_predictions(a, b, x, explainer)
+
+    return completeness_score(predictions_a, predictions_b)
+
+
 def centroids_displacements(a: TPrototypes, b: TPrototypes, penalty: float = 10.0) -> dict[int | str, float]:
+    """Calculate the displacement of the centroids between two sets of prototypes.
+
+    :param a: First set of prototypes.
+    :type a: TPrototypes
+    :param b: Second set of prototypes.
+    :type b: TPrototypes
+    :param penalty: Penalty for empty classes (default = 10.0).
+    :type penalty: float
+    """
+
     classes = set(a.keys()).union(b.keys())
 
     a = parse_prototypes(a)
@@ -58,6 +188,20 @@ def centroids_displacements(a: TPrototypes, b: TPrototypes, penalty: float = 10.
 
 
 def mean_centroid_displacement(a: TPrototypes, b: TPrototypes, penalty: float = 2.0) -> float:
+    """Calculate the mean displacement of the centroids between two sets of prototypes.
+
+    :param a: First set of prototypes.
+    :type a: TPrototypes
+    :param b: Second set of prototypes.
+    :type b: TPrototypes
+    :param penalty: Penalty for empty classes (default = 2.0).
+    :type penalty: float
+
+    :returns: The mean displacement of the centroids between the two sets of prototypes.
+    :rtype: float
+
+    """
+
     displacements = centroids_displacements(a, b, penalty)
     return np.mean(list(displacements.values()))
 
@@ -71,6 +215,20 @@ def _l2_distance_matrix(a: TPrototypes, b: TPrototypes, cls: int | float) -> np.
 
 
 def mean_minimal_distance(a: TPrototypes, b: TPrototypes, penalty: float = 2.0) -> float:
+    """Calculate the mean minimal distance between two sets of prototypes.
+
+    :param a: First set of prototypes.
+    :type a: TPrototypes
+    :param b: Second set of prototypes.
+    :type b: TPrototypes
+    :param penalty: Penalty for empty classes (default = 2.0).
+    :type penalty: float
+
+    :returns: The mean minimal distance between the two sets of prototypes.
+    :rtype: float
+
+    """
+
     classes = set(a.keys()).union(b.keys())
     distances = []
     for cls in classes:
@@ -92,6 +250,20 @@ def mean_minimal_distance(a: TPrototypes, b: TPrototypes, penalty: float = 2.0) 
 
 
 def classwise_mean_minimal_distance(a: TPrototypes, b: TPrototypes, penalty: float = 2.0) -> dict[int | str, float]:
+    """Calculate the class-wise mean minimal distance between two sets of prototypes.
+
+    :param a: First set of prototypes.
+    :type a: TPrototypes
+    :param b: Second set of prototypes.
+    :type b: TPrototypes
+    :param penalty: Penalty for empty classes (default = 2.0).
+    :type penalty: float
+
+    :returns: The class-wise mean minimal distance between the two sets of prototypes.
+    :rtype: dict[int | str, float]
+
+    """
+    
     classes = set(a.keys()).union(b.keys())
     distances = {}
     for cls in classes:
